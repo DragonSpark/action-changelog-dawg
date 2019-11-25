f69b6f856c77572cb6031df99da4c9005262a75a
Mike-EEE <michael@dragonspark.network>
2019-11-25 09:33:27 -0500

    Adjusting script.

--- entrypoint.ps1
+++ entrypoint.ps1
@@ -10,47 +10,19 @@ function Generate-Changelog-Dawg {
 		[String]$Template
 	)
 	
-	$headers = @{ 
-	#	"authorization" = "Bearer $AccessToken"	
-		"content-type" = "application/json"
-	};
-	
-	
 	$uri = "https://api.github.com/repos/$Repository/releases"
-	Write-Host $uri
-	
-	# $token = ConvertTo-SecureString $env:INPUT_ACCESS_TOKEN -AsPlainText -Force
-	$response = Invoke-RestMethod $uri -Authentication Bearer -Token $AccessToken # -Headers $headers # | ConvertTo-Json
-	
-	# $response | Write-Host
-	
-	$temp = ConvertTo-Json $response
-	
-	Write-Host $temp
-	
-	# $parameters = @{ releases = $response } | ConvertTo-Json
-	# $parameters | Write-Host
-	Write-Host "============"
-	
-	
-	
-	#$response = Invoke-RestMethod $uri -Authentication Bearer -Token $AccessToken | Sort-Object published_at -Descending | ConvertTo-Json | ConvertFrom-Json
-	
-	#Write-Host "HELLO??? $($response.value)"
-	
-	# $output = ConvertTo-PoshstacheTemplate -InputString $Template -ParametersObject $parameters;
-	# $output | Write-Host
-	return $Template
+	$response = Invoke-RestMethod $uri -Authentication Bearer -Token $AccessToken | Sort-Object published_at -Descending
+	$parameters = @{ releases = $response } | ConvertTo-Json;
+	$result = ConvertTo-PoshstacheTemplate -InputString $Template -ParametersObject $parameters;
+	return $result
 }
 
 Install-Module Poshstache -Force
 
-<#
-
 $repository = @{ $true = $env:INPUT_REPOSITORY; $false = $env:GITHUB_REPOSITORY; }[[bool]$env:INPUT_REPOSITORY]
 $token = ConvertTo-SecureString $env:INPUT_ACCESS_TOKEN -AsPlainText -Force
 $result = Generate-Changelog-Dawg $token $repository $env:INPUT_TEMPLATE
-Write-Host "WHATTAP: $($result.Length)"
+
 if ($Error.Count)
 {
 	foreach ($e in $Error)
@@ -60,30 +32,3 @@ if ($Error.Count)
 	exit 1
 }
 echo "::set-output name=result::$result"
-#>
-$repository = @{ $true = $env:INPUT_REPOSITORY; $false = $env:GITHUB_REPOSITORY; }[[bool]$env:INPUT_REPOSITORY]
-$token = ConvertTo-SecureString $env:INPUT_ACCESS_TOKEN -AsPlainText -Force
-<#
-$repository = @{ $true = $env:INPUT_REPOSITORY; $false = $env:GITHUB_REPOSITORY; }[[bool]$env:INPUT_REPOSITORY]
-$uri = "https://api.github.com/repos/$repository/releases"
-Write-Host $uri
-$token = ConvertTo-SecureString $env:INPUT_ACCESS_TOKEN -AsPlainText -Force
-$response = Invoke-RestMethod $uri -Authentication Bearer -Token $token
-# $response
-#>
-
-"==============================================================="
-
-Generate-Changelog-Dawg $token $repository "asdf"
-
-"==============================================================="
-
-<#
-
-$headers = @{ 
-		"authorization" = "Bearer $($env:INPUT_ACCESS_TOKEN)"	
-		# "content-type" = "application/json"
-	};
-	
-
-#>
