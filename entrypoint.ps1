af94393f584d17d6953296b41881d173c6791e1f
Mike-EEE <michael@dragonspark.network>
2019-11-25 02:49:16 -0500

    Adjusting script.

--- entrypoint.ps1
+++ entrypoint.ps1
@@ -10,7 +10,8 @@ function Generate-Changelog-Dawg {
 		[String]$Template
 	)
 	
-	$response = Invoke-RestMethod https://api.github.com/repos/$Repository/releases -Authentication OAuth -Token $AccessToken | Sort-Object published_at -Descending | ConvertTo-Json | ConvertFrom-Json
+	# $response = Invoke-RestMethod https://api.github.com/repos/$Repository/releases -Authentication OAuth -Token $AccessToken | Sort-Object published_at -Descending | ConvertTo-Json | ConvertFrom-Json
+	$response = Invoke-RestMethod https://api.github.com/repos/$Repository/releases  | Sort-Object published_at -Descending | ConvertTo-Json | ConvertFrom-Json -Authentication OAuth -Token $AccessToken
 	ConvertTo-PoshstacheTemplate -InputString $Template -ParametersObject (@{ releases = $response.value } | ConvertTo-Json)
 }
 
@@ -18,4 +19,16 @@ Install-Module Poshstache -Force
 
 $repository = @{ $true = $env:INPUT_REPOSITORY; $false = $env:GITHUB_REPOSITORY; }[[bool]$env:INPUT_REPOSITORY]
 $token = ConvertTo-SecureString $env:INPUT_ACCESS_TOKEN -AsPlainText -Force
-Generate-Changelog-Dawg $token $env:GITHUB_REPOSITORY $env:INPUT_TEMPLATE
+$result = Generate-Changelog-Dawg $token $env:GITHUB_REPOSITORY $env:INPUT_TEMPLATE
+
+$error.Count
+
+"ERROR: $error"
+
+<#
+if($lastexitcode -ne 0)
+{
+	echo "::error file=app.js,line=10,col=15::Something went wrong"
+
+|
+#>
