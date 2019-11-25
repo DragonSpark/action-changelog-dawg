4cfc8ba89c43d75059911eff418ae9a6f6c76647
Mike-EEE <michael@dragonspark.network>
2019-11-25 02:36:22 -0500

    Adjusting script.

--- entrypoint.ps1
+++ entrypoint.ps1
@@ -2,18 +2,19 @@ function Generate-Changelog-Dawg {
 	Param(
 		[Parameter(Mandatory=$true)]
 		[SecureString]$AccessToken,
+	
 		[Parameter(Mandatory=$true)]
 		[String]$Repository,
+		
 		[Parameter(Mandatory=$true)]
-		[String]$Template)
+		[String]$Template
+	)
 	
 	$response = Invoke-RestMethod https://api.github.com/repos/$Repository/releases | Sort-Object published_at -Descending | ConvertTo-Json | ConvertFrom-Json -Authentication OAuth -Token $AccessToken
 	ConvertTo-PoshstacheTemplate -InputString $Template -ParametersObject (@{ releases = $response.value } | ConvertTo-Json)
 }
 
-# Install-Module Poshstache -Force
+Install-Module Poshstache -Force
 
 $repository = @{ $true = $env:INPUT_REPOSITORY; $false = $env:GITHUB_REPOSITORY; }[[bool]$env:INPUT_REPOSITORY]
-# $temp = [bool]$env:INPUT_SECURITY_TOKEN 
-"Repository: $repository - $env:INPUT_TEMPLATE"
-# Generate-Changelog-Dawg $env:GITHUB_REPOSITORY
+Generate-Changelog-Dawg $env:INPUT_ACCESS_TOKEN $env:GITHUB_REPOSITORY $INPUT_TEMPLATE
