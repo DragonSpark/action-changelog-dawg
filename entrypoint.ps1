ddc265c78ad205808587273dd60b7963a2f65fc2
Mike-EEE <michael@dragonspark.network>
2019-11-25 02:28:21 -0500

    Adjusting script.

--- entrypoint.ps1
+++ entrypoint.ps1
@@ -1,24 +1,19 @@
 function Generate-Changelog-Dawg {
-	Param([Parameter(Mandatory=$true)]
-		  [String]$Owner, 
-		  [Parameter(Mandatory=$true)]
-		  [String]$Repository,
-		  [String]$Template = @"
-{{#releases}}
-# [{{ name }}]({{ html_url }})
-> {{ published_at }} UTC
-##### ``{{ tag_name }}``
-
-{{ body }}
-{{/releases}}
-"@)
-	$response = Invoke-RestMethod https://api.github.com/repos/$Owner/$Repository/releases | Sort-Object published_at -Descending | ConvertTo-Json | ConvertFrom-Json
+	Param(
+		[Parameter(Mandatory=$true)]
+		[SecureString]$AccessToken,
+		[Parameter(Mandatory=$true)]
+		[String]$Repository,
+		[Parameter(Mandatory=$true)]
+		[String]$Template)
+	
+	$response = Invoke-RestMethod https://api.github.com/repos/$Repository/releases | Sort-Object published_at -Descending | ConvertTo-Json | ConvertFrom-Json -Authentication OAuth -Token $AccessToken
 	ConvertTo-PoshstacheTemplate -InputString $Template -ParametersObject (@{ releases = $response.value } | ConvertTo-Json)
 }
 
 # Install-Module Poshstache -Force
 
 $repository = @{ $true = $env:INPUT_REPOSITORY; $false = $env:GITHUB_REPOSITORY; }[[bool]$env:INPUT_REPOSITORY]
-$temp = [bool]$env:INPUT_SECURITY_TOKEN 
-"Repository: $repository - $temp"
+# $temp = [bool]$env:INPUT_SECURITY_TOKEN 
+"Repository: $repository - $env:INPUT_TEMPLATE"
 # Generate-Changelog-Dawg $env:GITHUB_REPOSITORY
